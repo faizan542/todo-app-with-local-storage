3242a4086410f42e103130eb121394150bc059c3
faizan542 <faizansilat100@gmail.com>
2022-07-30 23:01:12 +0500

    Get todo function

--- todos/todo.js
+++ todos/todo.js
@@ -15,8 +15,28 @@ var userInfo = document.getElementById('userInfo');
 userInfo.innerHTML = getInfo.name;
 
 
-
 function logout(){
     localStorage.removeItem('loggedInUser');
     window.location.replace('file:///D:/js%20applications/todo-app/authantication/login/login.html');
+}
+
+
+var addBtn = document.getElementById('add-btn');
+var addTodoTitle = document.getElementById('todo-title');
+var addTodoText = document.getElementById('todo-text');
+var alerts = document.getElementById('alert');
+var msg = "";
+
+var todoArray = [];
+var todoObj = {
+    title: addTodoTitle.value,
+    text: addTodoText.value,
+}
+
+function addTodos(){
+    if(addTodoTitle.value === "" || addTodoText === ""){
+        alerts.style.display = "block";
+        msg = "Add titile and Description";
+        alerts.innerHTML = msg;
+    }
 }
